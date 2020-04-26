edf1bdc76bb58e8c45588e8d375178bfebb7a716
unknown <yoyoanusingh27@gmail.com>
2020-04-27 00:07:20 +0530

    chat layout

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -45,7 +45,7 @@ class Chat extends React.Component {
   getSent(message) {
     var list = this.state.chatList;
     list.push(<div className="cover"><div className="sentMes">
-      <image src="https://www.w3schools.com/tags/smiley.gif"/>
+      <img className="gif-size" src="https://www.w3schools.com/tags/smiley.gif"/>
       {message}
       <a href="https://www.w3schools.com/tags/smiley.gif">Here</a>
     </div></div>);
@@ -57,7 +57,10 @@ class Chat extends React.Component {
     <div className="topBar">
       GrayBot
     </div>
-    {this.state.chatList}
+    <div className="chatConv">
+      {this.state.chatList}
+    </div>
+    
     </div>);
   }
 }
